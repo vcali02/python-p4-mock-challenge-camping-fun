e37c28ec219629a363c0b635b6891013d5c263bb
valeria caliguire <caliguireval@gmail.com>
2023-06-19 16:34:02 -0600

    completed

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'instance/app.db')}")
 
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, abort
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -39,29 +39,29 @@ class Campers(Resource):
         )
         return res
 
-    # #POST /campers
-    # def post(self):
-    #     #1. set data as JSON
-    #     data = request.get_json()
-    #     #2. create an instance
-    #     new_camper = Camper(
-    #         name=data.get('camper'),
-    #         age=data.get('age')
-    #     )
-    #     #exceptions
-    #     if not new_camper:
-    #         abort(404, 'Validation error')
-    #     #3. add/commit
-    #     db.session.add(new_camper)
-    #     db.session.commit()
-    #     #4. dictionary
-    #     new_camper_dict = new_camper.to_dict()
-    #     #5. res
-    #     res = make_response(
-    #         new_camper_dict,
-    #         201
-    #     )
-    #     return res
+    #POST /campers
+    def post(self):
+        #1. set data as JSON
+        data = request.get_json()
+        #2. create an instance
+        new_camper = Camper(
+            name=data.get('name'),
+            age=data.get('age')
+        )
+        #exceptions
+        if not new_camper:
+            abort(404, 'Validation error')
+        #3. add/commit
+        db.session.add(new_camper)
+        db.session.commit()
+        #4. dictionary
+        new_camper_dict = new_camper.to_dict()
+        #5. res
+        res = make_response(
+            new_camper_dict,
+            201
+        )
+        return res
     
 
 #param1= name of resource class
@@ -73,10 +73,10 @@ api.add_resource(Campers, '/campers')
 class OneCamper(Resource):
     def get(self, id):
         #1. query
-        camper = Camper.query.filter_by(id = id).first()
+        camper = Camper.query.filter_by(id=id).first()
         #exception
         if not camper:
-            abort('Camper not found', 404)
+            abort(404, 'Camper not found')
         #2. dict
         camper_dict = camper.to_dict()
         #3. res
@@ -107,30 +107,30 @@ api.add_resource(Activities, '/activities')
 
 
 
-# #DELETE /activities/int:id
-# class OneActivity(Resource):
-#     def get(self, id):
-#         #1. query
-#         activity = Activity.query.filter_by(id = id).first()
-#         #2. dict
-#         activity_dict = [activity.to_dict()] 
-#         #3. res
-#         res = make_response(
-#             activity_dict,
-#             200
-#         )
-#         return res
-# #DELETE /activities/int:id
-#     def delete(self, id):
-#         #1. get activity by id
-#         activity = Activity.query.filter_by(id = id).first()
-#         #2. delete/commit
-#         db.session.delete(activity)
-#         db.session.commit()
-#         #3. return empty response
-#         return make_response({}, 204)
+#DELETE /activities/int:id
+class OneActivity(Resource):
+    def get(self, id):
+        #1. query
+        activity = Activity.query.filter_by(id = id).first()
+        #2. dict
+        activity_dict = activity.to_dict() 
+        #3. res
+        res = make_response(
+            activity_dict,
+            200
+        )
+        return res
+#DELETE /activities/int:id
+    def delete(self, id):
+        #1. get activity by id
+        activity = Activity.query.filter_by(id = id).first()
+        #2. delete/commit
+        db.session.delete(activity)
+        db.session.commit()
+        #3. return empty response
+        return make_response({}, 204)
 
-# api.add_resource(Activity, '/activities/<int:id>')
+api.add_resource(OneActivity, '/activities/<int:id>')
 
 
 
